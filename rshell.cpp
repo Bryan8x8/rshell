8465edd176f596e9e94a94623ceffcff9d77c140
Bryan8x8 <bryandelgado99@yahoo.com>
2018-03-08 05:55:08 -0800

    Update rshell.cpp

--- rshell.cpp
+++ rshell.cpp
@@ -11,10 +11,28 @@ using namespace std;
 
 int main() {
     Controller control;
+    Packager parse;
     bool breakLoop = false;
     std::cout << "Welcome to rshell, enter command 'terminate' to end your session." << std::endl;
+    string holdInput;
     while(!breakLoop){
-    breakLoop = control.receiveAndRun(); 
-    }          
+       std::cout<<"$ ";
+       holdInput = control.receiveInput();
+       holdInput = control.handleComment(holdInput);
+       //cout << "Term Flag: " << control.returnTerm() << endl;
+           if(control.properParen(holdInput))
+           {
+            control.execParen(holdInput);
+           }
+           else
+           {
+               cout << "Error: Improper Parenthesis" << endl;
+           }
+       
+       if((holdInput == "terminate") || (control.returnTerm())){
+           //cout<<"hit?"<<endl;
+           breakLoop = true;
+       }
+    }
     return 0;
 }
